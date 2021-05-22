98ee0684157beae98ea712f7c4aa41e908c847f1
Ivan Franchin <ivan.franchin@takeaway.com>
2021-05-22 15:29:07 +0200

    project update - update spring-boot to 2.5.0; - update spring-native to 0.10.0-SNAPSHOT.

--- docker-build.sh
+++ docker-build.sh
@@ -4,5 +4,5 @@ if [ "$1" = "native" ];
 then
   ./mvnw clean spring-boot:build-image --projects simple-service
 else
-  ./mvnw clean package jib:dockerBuild --projects simple-service
+  ./mvnw clean compile jib:dockerBuild --projects simple-service
 fi
