5980656fa6c007d233de6b0b5d750009b42f2d0c
Ivan Franchin <ivan.franchin@takeaway.com>
2021-12-17 11:56:50 -0300

    project update - update to spring-native 0.11.0; - update to springdoc-openapi 1.6.1.

--- docker-build.sh
+++ docker-build.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "native" ];
 then
-  ./mvnw clean spring-boot:build-image --projects simple-service
+  ./mvnw clean spring-boot:build-image --projects simple-service -DskipTests
 else
   ./mvnw clean compile jib:dockerBuild --projects simple-service
 fi
