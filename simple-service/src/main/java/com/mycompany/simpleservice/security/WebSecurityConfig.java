17d5adccba1b335274bf757b37041d498ce39159
Ivan Franchin <ivan.franchin@takeaway.com>
2022-06-27 16:21:49 +0200

    fix token issuer check

--- simple-service/src/main/java/com/mycompany/simpleservice/security/WebSecurityConfig.java
+++ simple-service/src/main/java/com/mycompany/simpleservice/security/WebSecurityConfig.java
@@ -1,11 +1,19 @@
 package com.mycompany.simpleservice.security;
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
+import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @EnableWebSecurity
 public class WebSecurityConfig {
@@ -17,7 +25,8 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                            JwtIssuerAuthenticationManagerResolver jwtIssuerAuthenticationManagerResolver) throws Exception {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/api/private").hasRole("USER")
                 .antMatchers(HttpMethod.GET, "/api/public").permitAll()
@@ -25,10 +34,22 @@ public class WebSecurityConfig {
                 .antMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated();
         http.oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(jwtAuthConverter);
+                .authenticationManagerResolver(jwtIssuerAuthenticationManagerResolver);
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.cors().and().csrf().disable();
         return http.build();
+    }
+
+    @Bean
+    JwtIssuerAuthenticationManagerResolver jwtIssuerAuthenticationManagerResolver(OAuth2ResourceServerProperties oAuth2ResourceServerProperties) {
+        Map<String, AuthenticationManager> authenticationManagers = new HashMap<>();
+        addManager(oAuth2ResourceServerProperties.getJwt().getIssuerUri(), authenticationManagers);
+        return new JwtIssuerAuthenticationManagerResolver(authenticationManagers::get);
+    }
+
+    private void addManager(String issuer, Map<String, AuthenticationManager> authenticationManagers) {
+        JwtAuthenticationProvider authenticationProvider = new JwtAuthenticationProvider(JwtDecoders.fromOidcIssuerLocation(issuer));
+        authenticationProvider.setJwtAuthenticationConverter(jwtAuthConverter);
+        authenticationManagers.put(issuer, authenticationProvider::authenticate);
     }
 }
