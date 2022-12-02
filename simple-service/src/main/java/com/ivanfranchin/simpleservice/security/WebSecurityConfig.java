917829da9e2fa5aec8c487d3b445f5e8ddeac016
Ivan Franchin <ivan.franchin@takeaway.com>
2022-12-02 16:59:07 -0300

    upgrade to keycloak 20.0.1 and code cleanup

--- simple-service/src/main/java/com/ivanfranchin/simpleservice/security/WebSecurityConfig.java
+++ simple-service/src/main/java/com/ivanfranchin/simpleservice/security/WebSecurityConfig.java
@@ -1,14 +1,10 @@
 package com.ivanfranchin.simpleservice.security;
 
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtValidators;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -34,12 +30,5 @@ public class WebSecurityConfig {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.cors().and().csrf().disable();
         return http.build();
-    }
-
-    @Bean
-    public JwtDecoder jwtDecoder(OAuth2ResourceServerProperties oAuth2ResourceServerProperties) {
-        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(oAuth2ResourceServerProperties.getJwt().getJwkSetUri()).build();
-        jwtDecoder.setJwtValidator(JwtValidators.createDefaultWithIssuer(oAuth2ResourceServerProperties.getJwt().getIssuerUri()));
-        return jwtDecoder;
     }
 }
