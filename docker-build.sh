f5bb6f854f6b8f6e3b69d7b24c638d7ee25470e2
Ivan Franchin <ivan.franchin@takeaway.com>
2024-11-07 23:13:22 +0100

    Replace Jib with Paketo Buildpacks and change Keycloak healthcheck in docker-compose

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+DOCKER_IMAGE_PREFIX="ivanfranchin"
+APP_NAME="simple-service"
+APP_VERSION="1.0.0"
+DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${APP_NAME}:${APP_VERSION}"
+SKIP_TESTS="true"
+
 if [ "$1" = "native" ];
 then
-  ./mvnw clean -Pnative spring-boot:build-image --projects simple-service -DskipTests
+  ./mvnw -Pnative clean spring-boot:build-image --projects "$APP_NAME" -DskipTests="$SKIP_TESTS" -Dspring-boot.build-image.imageName="$DOCKER_IMAGE_NAME"
 else
-  ./mvnw clean compile jib:dockerBuild --projects simple-service
+  ./mvnw clean spring-boot:build-image --projects "$APP_NAME" -DskipTests="$SKIP_TESTS" -Dspring-boot.build-image.imageName="$DOCKER_IMAGE_NAME"
 fi
